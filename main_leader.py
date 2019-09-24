e8d23e846ef4e238ca65f380f297b48fbcfd58b1
Quentin Coudert <qcoudert@utbm.fr>
2019-09-24 16:56:49 +0200

    +main_leader

---
+++ main_leader.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+import sys, brick_SL
+import pilot, distance_sensor, color_sensor, robot_status, lcd_display
+
+pilote_leader = pilot.Pilot()
+pilote_leader.forward(100)
