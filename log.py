1543f329ec01337570ed27e3e836ba4687092bc3
Quentin Coudert <qcoudert@utbm.fr>
2019-09-24 18:34:54 +0200

    Plusieurs logs

--- log.py
+++ log.py
@@ -1,10 +1,10 @@
-import csv
+import csv, time
 
 import sys
 
 class Log :
     def __init__(self, robot_status) :
-        self.file = open("logs.csv","w+")
+        self.file = open("logs"+ str(time.time()) + ".csv","w+")
         self.file.write("Time;Distance;Couleur\n")
         self.robot_status = robot_status
         
