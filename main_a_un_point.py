9f44f0eb32e9500861c0a179ca02edb596614ff7
Julien Medici <jmedici@utbm.fr>
2019-09-24 18:12:00 +0200

    main a un point and log

--- main_a_un_point.py
+++ main_a_un_point.py
@@ -8,10 +8,13 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 import sys, brick_SL
-import pilot, distance_sensor, color_sensor, robot_status, lcd_display
+import pilot, distance_sensor, color_sensor, robot_status, lcd_display, log
 
 pilote_suiveur = pilot.Pilot()
 distance_suiveur = distance_sensor.DistanceSensor()
+color_suiveur = color_sensor.ColorSensor()
+robot_suiveur = robot_status.RobotStatus(color_suiveur, distance_suiveur)
+log_suiveur = log.Log(robot_suiveur)
 
 D = 5
 Ts = 10
@@ -22,5 +25,6 @@ while True :
     dist = distance_suiveur.distance()
     vitesse = max(min(50, a*(dist-D)) , 0)
     pilote_suiveur.forwardRelative(vitesse)
+    log_suiveur.writeLog()
     wait(Ts)
     #vitesse : %(𝑡 + 𝑇𝑠) = max(min(50, 𝑎 × (𝑑(𝑡) − 𝐷)) , 0)
