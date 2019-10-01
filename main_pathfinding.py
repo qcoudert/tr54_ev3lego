130c824ce658c2c17fed4f75fbb71d6e67209140
Christopher Koenig <ckoenig@utbm.fr>
2019-10-01 18:31:51 +0200

    test learning

--- main_pathfinding.py
+++ main_pathfinding.py
@@ -9,16 +9,27 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 import sys, brick_SL
 import pilot, distance_sensor, color_sensor, robot_status, lcd_display
-
+import time
 
 pilote = pilot.Pilot()
 pilote_cs = color_sensor.CSensor()
+temoin = 0
+chemin = []
 
-while(1):
+
+while(temoin<2):
+    if(path_color==Color.GREEN):
+        temoin = temoin + 1
     path_color = pilote_cs.color()
     if(path_color==Color.WHITE):
-        pilote.forwardTurn(70, 350)
+        pilote.forwardTurn(90, 350)
+        chemin.append(1)
     elif(path_color==Color.BLUE):
         pilote.forwardRelative(90)
+        chemin.append(2)
     elif(path_color==Color.BLACK):
-        pilote.forwardTurn(70, -350)
+        pilote.forwardTurn(90, 350)
+        chemin.append(3)
+
+for i in chemin:
+    
