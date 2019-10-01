256a2c9e50d97ffffc5136809fb9ca685ee74576
Quentin Coudert <qcoudert@utbm.fr>
2019-10-01 16:32:04 +0200

    *Changement intégration vitesse dans logs

--- main.py
+++ main.py
@@ -15,27 +15,19 @@ import os
 brick.sound.beep(200, 100)
 print(sys.version)
 
-<<<<<<< HEAD
-pilote_leader = pilot.Pilot()
-isMoving = False
-while True:
-    if isMoving:
-        pilote_leader.stop()
-        isMoving = False
-    else:
-        pilote_leader.forwardRelative(40)
-        isMoving = True
-    wait(3000)
-=======
 pilote_suiveur_run = pilot.Pilot()
+pilote_suiveur_color = color_sensor.CSensor()
 pilote_suiveur_distance = distance_sensor.DistanceSensor()
+pilote_suiveur_status = robot_status.RobotStatus(pilote_suiveur_color.color(), pilote_suiveur_distance.distance(), 0)
 
 while(1):
     dist = pilote_suiveur_distance.distance()
+    vitesse = 50
     pilote_suiveur_run.forwardRelative(50)
     print(dist)
     if(dist<=29):
+        vitesse = 0
         pilote_suiveur_run.stop()
+    pilote_suiveur_status.updateStatus(pilote_suiveur_color.color(), pilote_suiveur_distance.distance(), vitesse)
 
 
->>>>>>> 291bf0cdc0848e139ca8253cde1e27e8ff6267e2
