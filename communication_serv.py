2768a5b96b4a298925ad62da08a06fc890c8b10d
Quentin Coudert <qcoudert@utbm.fr>
2019-10-15 19:15:52 +0200

    Communication server fait \o/

--- communication_serv.py
+++ communication_serv.py
@@ -1,12 +1,15 @@
 import socket
+from pybricks.tools import print
 
 class Server:
 
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.sock.settimeout(0.2)
-        self.sock.bind(("", 44444))
 
     def sendMsg(self, msg):
-        self.sock.sendto(msg, ('<broadcast>', 37020))
+        try:
+            self.sock.sendto(msg, ("192.168.43.255", 37020))
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            self.sock.close()
--- main.py
+++ main.py
@@ -8,26 +8,18 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 import sys, brick_SL
-import pilot, distance_sensor, color_sensor, robot_status, lcd_display
+import pilot, distance_sensor, color_sensor, robot_status, lcd_display, communication_serv
 import os
 
 # Write your program here
 brick.sound.beep(200, 100)
 print(sys.version)
-
-pilote_suiveur_run = pilot.Pilot()
-pilote_suiveur_color = color_sensor.CSensor()
-pilote_suiveur_distance = distance_sensor.DistanceSensor()
-pilote_suiveur_status = robot_status.RobotStatus(pilote_suiveur_color.color(), pilote_suiveur_distance.distance(), 0)
-
+i = 0
+serv = communication_serv.Server()
 while(1):
-    dist = pilote_suiveur_distance.distance()
-    vitesse = 50
-    pilote_suiveur_run.forwardRelative(50)
-    print(dist)
-    if(dist<=29):
-        vitesse = 0
-        pilote_suiveur_run.stop()
-    pilote_suiveur_status.updateStatus(pilote_suiveur_color.color(), pilote_suiveur_distance.distance(), vitesse)
+    serv.sendMsg("Slt"+str(i))
+    brick.display.text("Message envoye")
+    wait(1000)
+    i=i+1
 
 
