c10a336003d7f27c0db611759d0259f2e1475601
Pa2La <55575628+Pa2La@users.noreply.github.com>
2019-10-22 15:37:27 +0200

    Update communication.py

--- communication.py
+++ communication.py
@@ -1,14 +1,21 @@
 import socket
+from threading import Thread
 
-class Communication:
+class Communication(Thread):
 
     def __init__(self):
+        Thread.__init__(self)
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.sockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock.bind(("", 37020))
 
         self.rcvData = ["EOS"]
 
     def listen(self):
-        data, addr = self.sock.recvfrom(1024)
-        self.rcvData.append(data)
+        return self.rcvData
+    
+    def run(self):
+        while True: 
+            data, addr = self.sock.recvfrom(1024)
+            self.rcvData.append(data)
