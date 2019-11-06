046e7c51127fa6fe3aceedc54c44b0680726a6ce
Quentin <quentin.coudert@utbm.fr>
2019-11-06 17:29:24 +0100

    Organisation and Network rework
    Légère réorganisation des fichiers et ajout du fichier network.py contenant les classes à utiliser pour l'écoute et l'envoi de messages

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ print(sys.version_info)
 i = 0
 serv = communication_serv.Server("192.168.43.238")
 serv.start()
-isSending = False
+isSending = True
 rep = 0
 while(1):
     data = serv.getMsg()
     brick.display.text(data)
     #print(str(serv.receivedMutex))
     #print(str(serv.toSendMutex))
-    iserv.queueMsg("test")
+    serv.queueMsg("start")
     serv.sendMsg()
 
-    wait(1000)
+    wait(100)
 
 
